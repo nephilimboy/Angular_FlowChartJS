46f1152b141f941bdf3a304f0a8a6e47fa677ae8
amir <he.amirhossein@gmail.com>
2019-02-28 11:30:35 +0330

    add more operate

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Angular 6 + Jquery flowchart 
 - http://sebastien.drouyer.com/jquery.flowchart-demo/
 
+## Run
+- npm install
+- npm start
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,52 +7,69 @@ declare var $: any;
   templateUrl: "./app.component.html",
 })
 export class AppComponent implements AfterViewInit {
-
-  data = {
-    operators: {
-      operator: {
-        top: 20,
-        left: 20,
-        properties: {
-          title: 'Operator',
-          inputs: {
-            input_1: {
-              label: 'Input 1',
-            },
-            input_2: {
-              label: 'Input 2',
-            }
-          },
-          outputs: {
-            output_1: {
-              label: 'Output 1',
-            },
-            output_2: {
-              label: 'Output 2',
-            },
-            output_3: {
-              label: 'Output 3',
-            }
-          }
-        }
-      }
-    }
-  };
+  
+  // data = {
+  //   operators: {
+  //     operator1: {
+  //       top: 20,
+  //       left: 20,
+  //       properties: {
+  //         title: 'Operator 1',
+  //         class: 'myTest2',
+  //         inputs: {},
+  //         outputs: {
+  //           output_1: {
+  //             label: 'Output 1',
+  //           }
+  //         }
+  //       }
+  //     },
+  //     operator2: {
+  //       top: 80,
+  //       left: 300,
+  //       properties: {
+  //         title: 'Operator 2',
+  //         class: 'myTest2',
+  //         inputs: {
+  //           input_1: {
+  //             label: 'Input 1',
+  //           },
+  //           input_2: {
+  //             label: 'Input 2',
+  //           },
+  //         },
+  //         outputs: {}
+  //       }
+  //     },
+  //   },
+  //   links: {
+  //     link_1: {
+  //       fromOperator: 'operator1',
+  //       fromConnector: 'output_1',
+  //       toOperator: 'operator2',
+  //       toConnector: 'input_2',
+  //     },
+  //   }
+  // };
 
   @ViewChild('exampleDiv') exampleDiv: ElementRef;
 
   constructor() {
-    
+
   }
 
 
   ngAfterViewInit() {
     setTimeout(() => {
+
+
+
+
       $(this.exampleDiv.nativeElement).flowchart({
-        data: this.data
+        data: ''
       });
     }, 1000);
-    
+
   }
 
   // test(){
@@ -62,4 +79,66 @@ export class AppComponent implements AfterViewInit {
   //   });
   // }
 
+  operatorI = 0;
+  operatorII = 0;
+  addNewOperator() {
+
+    var operatorId = 'created_operator_' + this.operatorI;
+    var operatorData = {
+      top: 60,
+      left: 500,
+      properties: {
+        title: 'Operator ' + (this.operatorI + 3),
+        class: 'myTest',
+        inputs: {},
+        outputs: {
+          output_1: {
+            label: 'Output 1',
+          },
+          output_2: {
+            label: 'Output 2',
+          },
+          output_3: {
+            label: 'Output 3',
+          },
+          output_4: {
+            label: 'Output 4',
+          },
+        }
+      }
+    }
+
+    this.operatorI++;
+    $(this.exampleDiv.nativeElement).flowchart('createOperator', operatorId, operatorData);
+  }
+
+  addNewOperator2() {
+
+    var operatorId = 'created_operator_' + this.operatorI;
+    var operatorData = {
+      top: 60,
+      left: 500,
+      properties: {
+        title: 'Operator ' + (this.operatorI + 3),
+        class: 'myTest2',
+        inputs: {
+          input_1: {
+            label: 'Input 1',
+          },
+          input_2: {
+            label: 'Input 2',
+          },
+          input_3: {
+            label: 'Input 3',
+          },
+        },
+        outputs: {}
+      }
+    }
+
+    this.operatorI++;
+    $(this.exampleDiv.nativeElement).flowchart('createOperator', operatorId, operatorData);
+  }
+
 }
+
