18f085976c4131534197843f506681e780767111
amir <he.amirhossein@gmail.com>
2019-03-03 13:05:09 +0330

    fix zooming and pan + fix js dependency and removed the npm jquery libs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,76 +19,85 @@ export class AppComponent implements AfterViewInit {
 
   public diagModel: any;
 
-  // data = {
-  //   operators: {
-  //     operator1: {
-  //       top: 20,
-  //       left: 20,
-  //       properties: {
-  //         title: 'Operator 1',
-  //         class: 'myTest2',
-  //         inputs: {},
-  //         outputs: {
-  //           output_1: {
-  //             label: 'Output 1',
-  //           }
-  //         }
-  //       }
-  //     },
-  //     operator2: {
-  //       top: 80,
-  //       left: 300,
-  //       properties: {
-  //         title: 'Operator 2',
-  //         class: 'myTest2',
-  //         inputs: {
-  //           input_1: {
-  //             label: 'Input 1',
-  //           },
-  //           input_2: {
-  //             label: 'Input 2',
-  //           },
-  //         },
-  //         outputs: {}
-  //       }
-  //     },
-  //   },
-  //   links: {
-  //     link_1: {
-  //       fromOperator: 'operator1',
-  //       fromConnector: 'output_1',
-  //       toOperator: 'operator2',
-  //       toConnector: 'input_2',
-  //     },
-  //   }
-  // };
+  private cx: number;
+  private cy: number;
+
+
 
   @ViewChild('exampleDiv') exampleDiv: ElementRef;
 
-  constructor() {
 
+
+
+  constructor() {
   }
 
 
+
   ngAfterViewInit() {
-    setTimeout(() => {
 
 
+    var container = $('#chart_container');
+    this.cx = $('#exampleDiv').width() / 2;
+    this.cy = $('#exampleDiv').height() / 2;
+    $('#exampleDiv').panzoom({
+    });
+    $('#exampleDiv').panzoom('pan', -this.cx + container.width() / 2, -this.cy + container.height() / 2);
 
-
-      $(this.exampleDiv.nativeElement).flowchart({
-        data: ''
+    var possibleZooms = [0.5, 0.75, 1, 2, 3];
+    var currentZoom = 2;
+    container.on('mousewheel.focal', function (e) {
+      e.preventDefault();
+      var delta = (e.delta || e.originalEvent.wheelDelta) || e.originalEvent.detail;
+      var zoomOut: any = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+      currentZoom = Math.max(0, Math.min(possibleZooms.length - 1, (currentZoom + (zoomOut * 2 - 1))));
+      $('#exampleDiv').flowchart('setPositionRatio', possibleZooms[currentZoom]);
+      $('#exampleDiv').panzoom('zoom', possibleZooms[currentZoom], {
+        animate: false,
+        focal: e
       });
+
+    });
+
+
+    setTimeout(() => {
+      $(this.exampleDiv.nativeElement).flowchart({
+        data: '',
+        multipleLinksOnOutput: true,
+      });
+
     }, 1000);
 
   }
 
-  // test(){
-  //   // $('#exampleDiv').flowchart({
-  //     $(this.exampleDiv.nativeElement).flowchart({
-  //     data: this.data
-  //   });
-  // }
+
+  getOperatorData($element) {
+    var nbInputs = parseInt($element.data('nb-inputs'));
+    var nbOutputs = parseInt($element.data('nb-outputs'));
+    var data = {
+      properties: {
+        title: $element.text(),
+        inputs: {},
+        outputs: {}
+      }
+    };
+
+    var i = 0;
+    for (i = 0; i < nbInputs; i++) {
+      data.properties.inputs['input_' + i] = {
+        label: 'Input ' + (i + 1)
+      };
+    }
+    for (i = 0; i < nbOutputs; i++) {
+      data.properties.outputs['output_' + i] = {
+        label: 'Output ' + (i + 1)
+      };
+    }
+
+    return data;
+  }
+
+
 
   operatorI = 0;
   operatorII = 0;
@@ -96,8 +105,8 @@ export class AppComponent implements AfterViewInit {
 
     var operatorId = 'created_operator_' + this.operatorI;
     var operatorData = {
-      top: 60,
-      left: 500,
+      top: this.cx,
+      left: this.cy,
       properties: {
         title: 'Operator ' + (this.operatorI + 3),
         class: 'myTest',
@@ -127,8 +136,8 @@ export class AppComponent implements AfterViewInit {
 
     var operatorId = 'created_operator_' + this.operatorI;
     var operatorData = {
-      top: 60,
-      left: 500,
+      top: this.cx,
+      left: this.cy,
       properties: {
         title: 'Operator ' + (this.operatorI + 3),
         class: 'myTest2',
